d3056429551af78941202745711c5d30320e7a5f
Rafii Ramii <info.rafyramy@gmail.com>
2019-03-14 12:10:46 +0300

    resolve the mime error by unImport css files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+        var menuIsOpen = false;
+        // var menuWrapper = $("#menu-item");
+        var menuWrapper = document.querySelector("#menu-item");
+        function toggleMenu(){
+            alert("ok")
+            // if (!menuIsOpen){
+            //     // menuWrapper.addClass("open");
+            //     menuWrapper.classList.add("open");
+            //     menuIsOpen = true;
+            // }
+            // else{
+            //     // menuWrapper.removeClass("open");
+            //     menuWrapper.classList.remove("open");
+            //     menuIsOpen = false
+        }
