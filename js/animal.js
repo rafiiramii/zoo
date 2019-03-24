f441f7f9cba365fb1fc2ebb606832f38e72959a6
Rafii Ramii <info.rafyramy@gmail.com>
2019-03-24 03:13:04 +0300

    Increased font size of the animal desc h5 in home page

--- js/animal.js
+++ js/animal.js
@@ -1,8 +1,8 @@
  
 $(document).ready(function() {
     $('#toggle_btn').click(function() {
-      $('#menu-item').show();
-        $('.animal-types, #dots').hide();
+      //$('#menu-item').show();
+        $('.animal-types, #dots,#menu-item').toggle(2000);
     // alert('hii');
  
     });
--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
  
 $(document).ready(function() {
    $('#toggle_btn').click(function() {
-    $('#menu-item').show();
-       $('.animal-desc').hide();
+      
+    //$('#menu-item').show();
+       $('.animal-desc, #menu-item').slideToggle("slow").slideOutRight(2000);
     
 
    });
